14d816175be1f954332aa58b08d9bfe50d60728a
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-06-30 22:18:10 +0200

    feat: add memory game

---
+++ settings.js
@@ -0,0 +1,14 @@
+const AppRoute = {
+  Game: 'game',
+  Results: 'results',
+  Initial: 'initial'
+};
+
+const GAME_TYPES = [
+  { type: 'cats', icon: 'cats', label: 'Котики' },
+  { type: 'flowers', icon: 'flowers', label: 'Цветочки' },
+  { type: 'cars', icon: 'cars', label: 'Машины' },
+];
+
+const TIMEOUT = 900;
+const RANDOMIZED = true;
